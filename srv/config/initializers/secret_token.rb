510cd76ac4995a161caa9ac5ab9ba19527baff8d
user <user@ubuntu.(none)>
2013-09-26 15:20:40 +1000

    Added bare basics of rails

---
+++ srv/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HomelySrv::Application.config.secret_key_base = 'e069499750fe1c6eae14c5fb22531fef6d0b0addae45e1320eb0efe8d611f51be450bacd669a89d35b98ebc8d661294483b122a028cf74ed6501b71ddf26ccf7'
