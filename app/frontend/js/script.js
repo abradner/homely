8091fef89b7a2655a8abd6276743415da79781f1
Beth Crane <bethc@cse.unsw.edu.au>
2013-09-30 20:47:41 +1000

    [#57218186] Started establish system design for JS and testing button click/touchend

--- app/frontend/js/script.js
+++ app/frontend/js/script.js
@@ -1,16 +1,47 @@
-var lightStatus = 0;
+var kitchenLightPowerStatus = 0;
+var livingRoomLightPowerStatus = 0;
 
 $(document).ready(
    function() {
 
-    $('#lightsOnOff').click(function () {
-      lightStatus ^= 1;
-      if (lightStatus == 0) {
-        $(this).removeClass("btn-success").addClass("btn-inverse");
+    $('.changeableSetting').on('click touchend', function () {
+      var id = $(this).attr("id");
+      //alert(id);
+      var status = id + "Status";
+      //alert(status);
+      window[status] ^= 1;
+      if (window[status] == 0) {
+        $("#"+id).removeClass("btn-success").addClass("btn-inverse");
       } else {
-        $(this).removeClass("btn-inverse").addClass("btn-success");
+        $("#"+id).removeClass("btn-inverse").addClass("btn-success");
       }
     });
 
    }
 );
+
+
+/*
+We have classes for each device type
+
+e.g. Light contains a power button, brightness level and colour
+
+Each device is tied to a div containing it - e.g. <div id="livingRoomLight">
+
+Information from server - json.
+- ID name of device
+- device type
+- device status (for each of the settings, colour/brightness/etc)
+
+Every device we get we add a div in of its type with id ID
+Every button/setting will be IDbuttonName and class <type, button> (e.g. id="livingRoomLightPower", class="light power")
+And all variables will be like nameStatus, e.g. powerStatus
+so when you click something it finds the object it belongs to and edits the nameStatus
+
+So each settingType (e.g. power) has a function which takes in an object and modifies that generically.
+
+What I want:
+
+
+
+*/
