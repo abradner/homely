bb9803c9885926ab919b7a760f21ee2edc1477f8
Alex Bradner <alex@bradner.net>
2013-10-03 15:09:24 +1000

    making serial fat controller more flexible

--- srv/lib/serial_fat_controller.rb
+++ srv/lib/serial_fat_controller.rb
@@ -4,8 +4,16 @@
 
 require "serialport"
 
+if ARGV.size < 1
+  STDERR.print <<EOF
+  Usage: ruby #{$0} /dev/ttyOfArduino
+EOF
+  exit(1)
+end
+
 #params for serial port
-port_str = "/dev/tty.usbserial-A5002rO5"  #may be different for you
+#port_str = "/dev/tty.usbserial-A5002rO5"  #may be different for you
+port_str = ARGV[0].to_s
 baud_rate = 9600
 data_bits = 8
 stop_bits = 1
@@ -13,6 +21,15 @@ parity = SerialPort::NONE
 
 sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
+#  Thread.new {
+#    while true do
+#      STDOUT.printf("%c", sp.getc)
+#    end
+#  }
+
+#  while (l = STDIN.gets) do
+#    sp.write(l.sub("\n", "\r"))
+#  end
 
 open("/dev/tty", "r+") { |tty|
   tty.sync = true
