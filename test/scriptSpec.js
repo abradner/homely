0881e336064c501bc7120b84bf8412932a9d6fde
Beth Crane <bethc@cse.unsw.edu.au>
2013-10-03 19:47:31 +1000

    [#57862006] Fixed the jasmine test error; made the variables global
    So that they were being added to the window.

--- test/scriptSpec.js
+++ test/scriptSpec.js
@@ -1,58 +1,58 @@
 describe("Light Object", function() {
     var l = new Light("id");
-    it("initialises the ID correctly", function() {
+    it("Initialises the ID correctly", function() {
         expect(l.id).toBe("id");
     });
 
-    it("initialises the power to 0", function() {
+    it("Initialises the power to 0", function() {
         expect(l.power).toBe(0);
     });
 
-    it("initialises the brightness to 0", function() {
+    it("Initialises the brightness to 0", function() {
         expect(l.brightness).toBe(0);
     });
 
-    it("initialises the colour to 0", function() {
+    it("Initialises the colour to 0", function() {
         expect(l.colour).toBe(0);
     });
 
-    it("toggles power correctly", function() {
+    it("Toggles power correctly", function() {
         l.togglePower();
         expect(l.power).toBe(1);
         l.togglePower();
         expect(l.power).toBe(0);
     });
 
-    it ("sets power correctly", function() {
+    it ("Sets power correctly", function() {
         l.setPower(1);
         expect(l.power).toBe(1);
     });
 
-    it ("bounds power correctly", function() {
+    it ("Bounds power correctly", function() {
         l.setPower(12);
         expect(l.power).toBe(1);
         l.setPower(-3);
         expect(l.power).toBe(0);
     });
 
-    it ("sets brightness correctly", function() {
+    it ("Sets brightness correctly", function() {
         l.setBrightness(5);
         expect(l.brightness).toBe(5);
     });
 
-    it ("bounds brightness correctly", function() {
+    it ("Bounds brightness correctly", function() {
         l.setBrightness(12);
         expect(l.brightness).toBe(9);
         l.setBrightness(-3);
         expect(l.brightness).toBe(0);
     });
 
-    it ("sets colour correctly", function() {
+    it ("Sets colour correctly", function() {
         l.setColour(32);
         expect(l.colour).toBe(32);
     });
 
-    it ("bounds colour correctly", function() {
+    it ("Bounds colour correctly", function() {
         l.setColour(293);
         expect(l.colour).toBe(255);
         l.setColour(-36);
@@ -61,36 +61,35 @@ describe("Light Object", function() {
 
 });
 
+var bathroomLight = new Light('bathroomLight');
+var livingRoomLight = new Light('livingRoomLight');
 describe("Handling clicks", function() {
-    //var kitchenLight = new Light('kitchenLight');
-    //var livingRoomLight = new Light('livingRoomLight');
-
 
     it ("Changes the value of the power status when the button is clicked", function() {
-        kitchenLight.setPower(0);
-        $("#kitchenLight_Power").click();
-        expect(kitchenLight.power).toBe(1);
+        bathroomLight.setPower(0);
+        $("#bathroomLight_Power").click();
+        expect(bathroomLight.power).toBe(1);
 
     });
 
     it("Only affects the element that is clicked", function() {
-        kitchenLight.setPower(0);
+        bathroomLight.setPower(0);
         livingRoomLight.setPower(0);
-        $("#kitchenLight_Power").click();
-        expect(kitchenLight.power).toBe(1);
+        $("#bathroomLight_Power").click();
+        expect(bathroomLight.power).toBe(1);
         expect(livingRoomLight.power).toBe(0);
-    });s
+    });
 
     it("Adjusts the power classes for turning off", function() {
-        kitchenLight.setPower(0);
-        expect($('#kitchenLight_Power')).toHaveClass('btn-inverse');
-        expect($('#kitchenLight_Power')).not.toHaveClass('btn-success');
+        bathroomLight.setPower(0);
+        expect($('#bathroomLight_Power')).toHaveClass('btn-inverse');
+        expect($('#bathroomLight_Power')).not.toHaveClass('btn-success');
     });
 
     it("Adjusts the power classes for turning on", function() {
-        kitchenLight.setPower(1);
-        expect($('#kitchenLight_Power')).toHaveClass('btn-success');
-        expect($('#kitchenLight_Power')).not.toHaveClass('btn-inverse');
+        bathroomLight.setPower(1);
+        expect($('#bathroomLight_Power')).toHaveClass('btn-success');
+        expect($('#bathroomLight_Power')).not.toHaveClass('btn-inverse');
     });
 
 });
