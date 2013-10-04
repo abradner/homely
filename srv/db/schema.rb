e08c5b40ffcc673b8119f33cd5dd0e7a3304e676
Alex Bradner <alex@bradner.net>
2013-10-05 00:34:56 +1000

    [#57219874] Adding (missed) schema

--- srv/db/schema.rb
+++ srv/db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131004121546) do
+
+  create_table "devices", force: true do |t|
+    t.string   "name"
+    t.string   "type"
+    t.string   "interface"
+    t.string   "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
