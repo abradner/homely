943f31b0efa95d21fdf1183e234375c754c1c14c
user <user@ubuntu.(none)>
2013-10-13 21:12:32 +1100

    Made some changes to hex

--- srv/app/controllers/pages_controller.rb
+++ srv/app/controllers/pages_controller.rb
@@ -1,56 +1,99 @@
 class PagesController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => :colour
+
   @state
   @colour
   def home
   end
 
   def red
-    set_colour "900"
+    set_colour "ff0000", true
   end
 
   def green
-    set_colour "090"
+    set_colour "00ff00", true
   end
 
   def blue
-    set_colour "009"
+    set_colour "0000ff", true
   end
 
   def off
-    set_colour "000"
+    set_colour "000000", false
   end
 
   def on
-    set_colour @colour
+    set_colour @@colour, false
   end
 
   def white
-    set_colour "999"
+    set_colour "ffffff", true
+  end
+
+  def to_rgb(colour)
+    result = []
+    result.append(colour.slice(0, 2).to_i(16))
+    result.append(colour.slice(2, 2).to_i(16))
+    result.append(colour.slice(4, 2).to_i(16))
+    return result
+  end
+
+  def to_hex(rgb)
+    res = ""
+    rgb.each do |v|
+      s = v.to_s(16)
+      while s.length < 2 do
+        s = "0" + s
+      end
+      res += s
+    end
+    return res
   end
 
   def darker
-    colours = @colour.bytes
-    colours.map! do |col|
-      col = col > 0 ? col-1 : 0
+    rgb = to_rgb(@@colour)
+    new_rgb = []
+    rgb.each do |v|
+      v -= 10
+      if v < 0
+        v = 0
+      end
+      new_rgb.append(v)
     end
-
-    set_colour(colours.join)
+    set_colour(to_hex(new_rgb), true)
   end
 
- def lighter
-    colours = @colour.bytes
-    colours.map! do |col|
-      col = col < 9 ? col+1 : 9
-    end
 
-    set_colour(colours.join)
+
+  def lighter
+    rgb = to_rgb(@@colour)
+    new_rgb = []
+    rgb.each do |v|
+      v += 10
+      if v > 255
+        v = 255
+      end
+      new_rgb.append(v)
+    end
+    set_colour(to_hex(new_rgb), true)
   end
 
 
   def colour
     colour = params[:colour]
-    set_colour(colour)
+    if colour =~ /(^\d{3}$)/
+      colours = colour.chars
+      new_colours=""
+      colours.each do |c|
+        v = (c.ord - '0'.ord)
+        s = ((v * 255)/9).to_s(16)
+        while s.length < 2 do
+          s = "0" + s
+        end
+        new_colours += s
+      end
+      set_colour(new_colours, true)
+    end
   end
 
   def query
@@ -58,10 +101,7 @@ class PagesController < ApplicationController
     display_state(id)
   end
 
-private
-
-
-  def set_colour(colour)
+  def set_colour(colour, store)
 
     require 'arduino_communicator/arduino_communicator'
     require 'arduino_communicator/serial_arduino_communicator'
@@ -70,7 +110,7 @@ private
     ard2 = SerialArduinoCommunicator.new
 
     message = "(0" + colour + ")"
-
+    printf("Message = %s\n", message)
     #ard1.send! message
     ard2.send! message
 
@@ -117,7 +157,6 @@ private
 
       @state << to_receive.chomp
     }
-
     #ard1.close
     ard2.close
     render(:'pages/basic_commands')
