aa1d87ce8c588238df666cde759789ebc976f606
user <user@ubuntu.(none)>
2013-10-13 21:24:20 +1100

    Made serialLED do hex

--- ard/SerialLEDDimmer/SerialLEDDimmer.ino
+++ ard/SerialLEDDimmer/SerialLEDDimmer.ino
@@ -42,6 +42,37 @@ void setup() {
   welcome();
 }
 
+int read_hex() {
+  int a = Serial.read();
+  int b = Serial.read();
+  if (a >= 'a') {
+    a = a - 'a' + 10;
+  } else {
+    a -= '0';
+  }
+  if (b >= 'a') {
+    b = b - 'a' + 10;
+  } else {
+    b -= '0';
+  }
+  return a * 16 + b;
+}
+
+void print_hex(int v) {
+  int a = v / 16;
+  int b = v % 16;
+  if (a < 10) {
+    Serial.print(a + '0');
+  } else {
+    Serial.print(a - 10 + 'a');
+  }
+  if (b < 10) {
+    Serial.print(b + '0');
+  } else {
+    Serial.print(b - 10 + 'a');
+  }
+}
+
 void loop() {
   if (Serial.available() > 1) {    
     if (Serial.peek() == '(') {
@@ -51,19 +82,15 @@ void loop() {
       Serial.read();
       int ledNo = Serial.read();
       // look for the next valid digit in the incoming serial stream:
-      int red = Serial.read(); 
+      int red = read_hex(); 
       // do it again:
-      int green = Serial.read(); 
+      int green = read_hex();
       // do it again:
-      int blue = Serial.read(); 
+      int blue = read_hex(); 
   
       Serial.read(); // ')'
     
-      red   = interpolate_colour(red);
-      green = interpolate_colour(green);
-      blue  = interpolate_colour(blue);
-   
-      ledNo = ledNo - ASCIIOFFSET;
+      ledNo = ledNo - '0';
       
       // fade the red, green, and blue legs of the LED: 
       Serial.print("Setting Colours - ");
@@ -89,9 +116,9 @@ void loop() {
       // TODO send the state
       Serial.print("(");
       Serial.print(ledNo);
-      Serial.print(interpolate_byte(red));
-      Serial.print(interpolate_byte(green));
-      Serial.print(interpolate_byte(blue));
+      print_hex(red);
+      print_hex(green);
+      print_hex(blue);
       Serial.println(")");
     } else {
       // Invalid data, gobble hoping that it was a typo
