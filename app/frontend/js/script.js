a25888f8070a22316a32eb6a34c3327f4520a911
Beth Crane <bethc@cse.unsw.edu.au>
2013-10-24 22:34:24 +1100

    Committed to the wrong branch. Remerging to commit to master.

--- app/frontend/js/script.js
+++ app/frontend/js/script.js
@@ -1,7 +1,27 @@
 /* General Script File*/
 var onMobile = false;
 
-// Callback for when settings are changed. Updates the server and the display appropriately.
+$(document).ready(function() {
+    // If Android exists then we're on the mobile app, in which case we use touch
+    if (window.Android) {
+        onMobile = true;
+    // If it doesn't then we set up a mock android (to remove errors if its called)
+    } else {
+        window.Android = new MockAndroid();
+    }
+
+    // Server URL
+    $('#appsettings').append(newServerURLField())
+    var server = Android.getServerUrl();
+
+    var serverUrl = 'http://'+ server + ':3000';
+    var fayeUrl = 'http://'+ server + ':9292/faye';
+
+    initialiseData(serverUrl);
+    startFaye(fayeUrl);
+});
+
+/* Callback for when settings are changed. Updates the server and the display appropriately. */
 var handleSettingUpdate = function (event) {
     var id = $(this).attr('id');
     capabilityId = $(this).data('capability-id');
@@ -12,45 +32,35 @@ var handleSettingUpdate = function (event) {
     cap.settings[id].updateToServer(id, value);
 }
 
-$(document).ready(function() {
-
-    // If Android exists then we're on the mobile app, in which case we use touch
-    if (window.Android) {
-        onMobile = true;
-        // Necessary for dynamically loaded content - can't just attach it to the class
-        $('#devices').on('touchend', '.changeableSetting', handleSettingUpdate);
-    // If it doesn't then we set up a mock android (to remove errors if its called)
-    } else {
-        window.Android = new MockAndroid();
-        $('#devices').on('click change', '.changeableSetting', handleSettingUpdate);
-    }
-
-    // Server URl
-    $('#appsettings').append(newServerURLField())
-    var server = Android.getServerUrl();
-
-    var serverUrl = 'http://'+ server + ':3000';
-    var fayeUrl = 'http://'+ server + ':9292/faye';
-
+/* Adding content to the page with server JSON and a template
+Also creates the device objects for our use*/
+var initialiseData = function(serverUrl) {
+    //Get the Template and compile it
+    var source   = $("#appTemplate").html();
+    var template = Handlebars.compile(source);
     // JSON object describing the devices we have access to
     $.getJSON(serverUrl+'/devices.json', function(data) {
-        // We create new device objects (incl. caps and settings), store and display them.
+        var html    = template(data);
+        //Replace the body section with the new code.
+        document.body.innerHTML = html;
+        // Make devices for each one - updates the page to correct values
         $.each(data, function (val) {
             addDevice(data[val], serverUrl);
+/* Charissa's Colour Wheel Stuff ... */
             var dataId = data[val]["id"];
             $('#devices').append(devices[dataId].stringDisplay());//newDeviceBox(devices[dataId]));
-            //For each device we find its settings and if they are a colour setting, create the colourwheel 
+            //For each device we find its settings and if they are a colour setting, create the colourwheel
             $.each(devices[dataId].capabilities, function (cap) {
                 var capabilityId = devices[dataId].capabilities[cap]["id"];
                 $.each(devices[dataId].capabilities[capabilityId].settings, function(set) {
                     var settingId = devices[dataId].capabilities[capabilityId].settings[set].id;
                     if (devices[dataId].capabilities[capabilityId].settings[settingId].name == "Colour") {
-                        var div = devices[dataId].capabilities[capabilityId].settings[settingId].divId; 
+                        var div = devices[dataId].capabilities[capabilityId].settings[settingId].divId;
                         var cw = Raphael.colorwheel($(div)[0], 75);
                         cw.input($(div + "_input")[0]);
                         devices[dataId].capabilities[capabilityId].settings[settingId].cw = cw;
                         // On change update the server
-                        cw.onchange(function(colour) 
+                        cw.onchange(function(colour)
                             {
                                 var div = cw.parent();
                                 var id = div.attr('id');
@@ -60,14 +70,25 @@ $(document).ready(function() {
                                 var cap = devices[deviceId].capabilities[capabilityId];
                                 var value = cap.settings[id].getChangedValue();
                                 cap.settings[id].updateToServer(id, value);
-                                
+
                             });
                     }
                 });
             });
+            /* End Charissa's Colour Wheel Stuff ...(I think) */
         });
+        // Enable callbacks for devices - must be done here because classes don't exist on page load
+        if (onMobile) {
+            $('#devices').on('touchend', '.changeableSetting', handleSettingUpdate);
+        } else {
+            $('#devices').on('click change', '.changeableSetting', handleSettingUpdate);
+        }
     });
 
+}
+
+/* Receives broadcasts from the server each time it updates */
+var startFaye = function(fayeUrl) {
     var client = new Faye.Client(fayeUrl);
     var subscription = client.subscribe('/connect', function(message) {
         message = $.parseJSON(message);
@@ -77,32 +98,14 @@ $(document).ready(function() {
         var value = message["value"]
         devices[deviceId].capabilities[capabilityId].settings[settingId].updateFromServer(value);
     });
-    
+}
 
+/* A set of templating helpers for printing out the correct setting type */
+Handlebars.registerHelper("isPower", function(settingName, options) {
+    var fnTrue=options.fn, fnFalse=options.inverse;
+    return settingName == 'Power' ? fnTrue() : fnFalse();
 });
-
-
-/*
-We have classes for each device type
-
-e.g. Light contains a power button, brightness level and colour
-
-Each device is tied to a div containing it - e.g. <div id="livingRoomLight">
-
-Information from server - json.
-- ID name of device
-- device type
-- device status (for each of the settings, colour/brightness/etc)
-
-Every device we get we add a div in of its type with id ID
-Every button/setting will be IDbuttonName and class <type, button> (e.g. id="livingRoomLightPower", class="light power")
-And all variables will be like nameStatus, e.g. powerStatus
-so when you click something it finds the object it belongs to and edits the nameStatus
-
-So each settingType (e.g. power) has a function which takes in an object and modifies that generically.
-
-What I want:
-
-
-
-*/
+Handlebars.registerHelper("isColour", function(settingName, options) {
+    var fnTrue=options.fn, fnFalse=options.inverse;
+    return settingName == 'Colour' ? fnTrue() : fnFalse();
+});
