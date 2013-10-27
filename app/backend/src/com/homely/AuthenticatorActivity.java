fabfcbabb6f3a012da42360adb87295c32358b1a
Kenneth Wong <k.kennyboy@gmail.com>
2013-10-27 16:31:37 +1100

    [#59324584] Token is now stored in SharedPreferences when login is successful

--- app/backend/src/com/homely/AuthenticatorActivity.java
+++ app/backend/src/com/homely/AuthenticatorActivity.java
@@ -17,10 +17,12 @@ import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 
-import com.homely.models.AuthToken;
+import org.json.JSONObject;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
 
 public class AuthenticatorActivity extends Activity {
 	private static final int AUTH_LOADER_ID = 0;
@@ -44,31 +46,49 @@ public class AuthenticatorActivity extends Activity {
 		findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				handleLoginAttempt();
+				if (handleLoginAttempt()) {
+					finish();
+				} else {
+					// Login attempt failed
+				}
 			}
 		});
 	}
 
-	private void handleLoginAttempt() {
+	private HttpPost formRequest(String server, String email, String password) throws UnsupportedEncodingException {
+		HttpPost httppost = new HttpPost("http://" + server + ":3000/tokens.json");
+
+		ArrayList<NameValuePair> postParameters;
+
+		postParameters = new ArrayList<NameValuePair>();
+		postParameters.add(new BasicNameValuePair("email", email));
+		postParameters.add(new BasicNameValuePair("password", password));
+
+		httppost.setEntity(new UrlEncodedFormEntity(postParameters));
+
+		return httppost;
+	}
+
+	private String getTokenFromJSON(String s) throws JSONException {
+		JSONObject jObject = new JSONObject(s);
+
+		// TODO don't hardcode this?
+		return jObject.getString("token");
+	}
+
+	// Returns true if successful and logged in.
+	//         false otherwise
+	private boolean handleLoginAttempt() {
 		String server = mServer.getText().toString();
 		String email = mEmail.getText().toString();
 		String password = mPassword.getText().toString();
-		// TODO replace with something more spinning wheel-y
+		// TODO replace with something more spinning wheel progress bar-y 
 		Toast.makeText(getApplicationContext(), "Handling login attempt with " + server + " " + email + " " + password, Toast.LENGTH_LONG).show();
 
 		try {
 			HttpClient httpclient = new DefaultHttpClient();
-			HttpPost httppost = new HttpPost("http://" + server + "/tokens.json");
+			HttpResponse response = httpclient.execute(formRequest(server, email, password));
 
-			ArrayList<NameValuePair> postParameters;
-
-			postParameters = new ArrayList<NameValuePair>();
-			postParameters.add(new BasicNameValuePair("email", email));
-			postParameters.add(new BasicNameValuePair("password", password));
-
-			httppost.setEntity(new UrlEncodedFormEntity(postParameters));
-
-			HttpResponse response = httpclient.execute(httppost);
 			StatusLine statusLine = response.getStatusLine();
 			if(statusLine.getStatusCode() == HttpStatus.SC_OK){
 				ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -77,15 +97,18 @@ public class AuthenticatorActivity extends Activity {
 				String responseString = out.toString();
 
 				// responseString is hopefully a json string with token in it
-
-				Toast.makeText(getApplicationContext(), responseString, Toast.LENGTH_LONG).show();
+				getSharedPreferences(PREFS_NAME, 0).edit().putString("token", getTokenFromJSON(responseString));
+				return true;
 			} else{
 				//Closes the connection.
 				response.getEntity().getContent().close();
 				Toast.makeText(getApplicationContext(), statusLine.getReasonPhrase(), Toast.LENGTH_LONG).show();
 			}
+		} catch (JSONException e) {
+			// JSON response is broken
 		} catch (Exception e) {
 			Toast.makeText(getApplicationContext(), "Connection with server " + server + "failed", Toast.LENGTH_LONG).show();
 		}
+		return false;
 	}
 }
